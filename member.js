716a055ed9084a46b1f31f2a579ecabec613c6d5
María León Téllez <94014309+MariaLT@users.noreply.github.com>
2023-11-07 16:55:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 25,
+        skills: ["JavaScript", "React", "Node"],
+        calculateNumbers: calculateNumbers(10, 20)
+    };
+    return member;
+}
